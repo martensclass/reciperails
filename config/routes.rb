726c8f9b61ef99ca0bdf30b5c1030814574ecb2b
JohnMartens <jmartens@scdsb.on.ca>
2015-06-17 18:48:44 +0000

    Changes style and added recipes route as a resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
  get "/home" => "pages#home"
  
  root "pages#home"
+ 
+ resources :recipes
+ #this gest the following
+ #index 
+ #show
+ #edit
+ #update
+ #new
+ #create
+ #delete
+ 
 end
