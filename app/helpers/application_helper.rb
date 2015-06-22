2f74cafeb18165f121b3bbad19a40f1e689372a4
JohnMartens <jmartens@scdsb.on.ca>
2015-06-22 18:06:36 +0000

    Added editing abilities

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    
+    def get_gravimage(chef, options = { size: 80 }) 
+       
+       hexemail = Digest::MD5::hexdigest(chef.email.downcase);
+       size = options[:size]
+       
+       img_url = "http://secure.gravatar.com/avatar/#{hexemail}?s=#{size}"
+       
+       image_tag(img_url, alt: chef.chefname, class: 'avimg')
+    end
+    
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ActiveRecord::Base
     belongs_to :chef
     validates :chef_id,     presence: true 
-    validates :name,        presence: true, length: { in: 5..30 }
-    validates :summary,     presence: true, length: { in: 10..100 }
-    validates :description, presence: true, length: { in: 10..300 }
+    validates :name,        presence: true, length: { in: 2..30 }
+    validates :summary,     presence: true, length: { in: 5..100 }
+    validates :description, presence: true, length: { in: 5..300 }
 end
